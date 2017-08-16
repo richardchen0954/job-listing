246f47be993aa44b65f084b9c539e4e5b44711ee
西影行 <richardchen0954@gmail.com>
2017-08-17 06:54:04 +0800

    implement step 5

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,8 +1,16 @@
 class Job < ApplicationRecord
-  class Job < ApplicationRecord
-    validates :title, presence: true
-    validates :wage_upper_bound, presence: true
-    validates :wage_lower_bound, presence: true
-    validates :wage_lower_bound, numericality: { greater_than: 0}
+  validates :title, presence: true
+  validates :wage_upper_bound, presence: true
+  validates :wage_lower_bound, presence: true
+  validates :wage_lower_bound, numericality: { greater_than: 0 }
+
+  def publish!
+    self.is_hidden = false
+    save
+  end
+
+  def hide!
+    self.is_hidden = true
+    save
   end
 end
