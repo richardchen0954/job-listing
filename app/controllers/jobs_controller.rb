8446f4c6897779b201a4befdabd1b4cff7e3b0f0
西影行 <richardchen0954@gmail.com>
2017-08-17 06:19:56 +0800

    implement wage part

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -44,11 +44,11 @@ before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destro
 
     redirect_to jobs_path
   end
-  
+
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 
 end
